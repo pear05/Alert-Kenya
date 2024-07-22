b5841504cdae9db09a0906437e31e3ce0ba363b8
pear_05 <waswaustin@gmail.com>
2024-07-22 14:02:16 +0300

    DB Config

---
+++ dbconfig.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost"; // or your database server IP
+$username = "root"; // your MySQL username
+$password = ""; // your MySQL password
+$dbname = "Alert_Kenya";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
